234d8604ae5606778b8eb742756cde9cbbd41e31
Rohit <rohitps008gmail.com>
2026-04-08 18:17:12 +0530

    added practice page and modified loginpage

---
+++ src/main/java/com/qa/opencart/pages/PracticePage.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.pages;
+
+public class PracticePage {
+
+	
+	int a=10;
+	
+	public void gitCheck()
+	{
+		System.out.println("This Method is to check Git functionality");
+	}
+	
+	
+}
