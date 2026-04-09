08e3898e61e97b242257c32adbddc21841a5da86
Rohitps0075 <94100840+Rohitps0075@users.noreply.github.com>
2026-04-09 12:02:43 +0530

    Create p14.java
    adding P14.java file

---
+++ src/main/java/com/qa/opencart/pages/p14.java
@@ -0,0 +1,25 @@
+package Programs_2;
+
+public class P14 {
+
+	public static void main(String[] args) {
+		//find the sum of odd numbers in a given number.
+		
+		int no=136;
+		int sum=0;
+				
+				while(no!=0)
+				{
+					int rem=no%10;
+					
+					if(rem%2==1)
+					{
+						sum=sum+rem;
+					}
+					no=no/10;
+					
+					
+				}
+				System.out.println(sum);
+	}
+}
